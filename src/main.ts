f2cb3251115eb66690547b1363ef41d75201425e
Daniel <70926662+Sacred29@users.noreply.github.com>
2021-03-14 10:55:13 +0800

    Cry-1 Deploy swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Crypto-plus backend')
+    .setDescription('The crypto-plus API to issue new green ICOs.')
+    .setVersion('1.0')
+    .addTag('Crypto-plus')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
